213d0e25466b4e67e15961f2b5379ec816277f49
John Lawson <80202595+john-lawson1@users.noreply.github.com>
2024-03-13 21:54:45 +0000

    more updates for issue 1

--- T17/submissions/task_manager.py
+++ T17/submissions/task_manager.py
@@ -389,18 +389,18 @@ def generate_reports():
             per_user_tasks_incomplete = 0
             per_user_overdue = 0
             # loop to increment count variables for specified conditions
-            for t in task_dict:
-                if t['username'] ==  k and t['completed'] == True:
+            for task in task_dict:
+                if task['username'] ==  k and task['completed'] == True:
                     count_complete += 1
                     count_user_tasks += 1
                     count_user_tasks_complete += 1
-                elif t['username'] ==  k and t['due_date'] < datetime.today():
+                elif task['username'] ==  k and task['due_date'] < datetime.today():
                     count_overdue += 1
                     count_incomplete += 1
                     count_user_tasks += 1
                     count_user_tasks_overdue += 1
                     count_user_tasks_incomplete += 1
-                elif t['username'] ==  k:
+                elif task['username'] ==  k:
                     count_incomplete += 1
                     count_user_tasks += 1
                     count_user_tasks_incomplete += 1
